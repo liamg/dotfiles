523f3a97a88c0396152631cde7c2479755f79039
Liam Galvin <liamgalvin@protonmail.com>
2021-08-17 22:07:59 +0100

    Wallpaper tweaks

--- wallpaper/install.sh
+++ wallpaper/install.sh
@@ -2,11 +2,16 @@
 
 set -e
 source core.sh
+wallpaper=https://w.wallhaven.cc/full/v9/wallhaven-v9m2r8.png
 
 # wallpaper
 log_info wallpaper "Setting..."
 if [[ -f wallpaper/custom.png ]]; then
     ln -sf `pwd`/wallpaper/custom.png $HOME/active-wallpaper
+elif [[ -f wallpaper/custom.jpg ]]; then
+    ln -sf `pwd`/wallpaper/custom.jpg $HOME/active-wallpaper
 else
-    ln -sf `pwd`/wallpaper/grid.png $HOME/active-wallpaper
+	[[ -f wallpaper/default.jpg ]] || (log_info wallpaper "Downloading..." && curl -sL -o wallpaper/default.jpg $wallpaper)
+    ln -sf `pwd`/wallpaper/default.jpg $HOME/active-wallpaper
 fi
+
