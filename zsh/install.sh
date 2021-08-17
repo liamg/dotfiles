b140f894505b9c85f1252b53be5c0809b7fcf148
Liam Galvin <liam.galvin@aquasec.com>
2021-08-17 14:49:07 +0100

    zsh config

--- zsh/install.sh
+++ zsh/install.sh
@@ -7,6 +7,14 @@ check_package_installed zsh
 
 CUSTOMDIR="$HOME/.oh-my-zsh/custom"
 
+log_info zsh "Checking default shell is zsh..."
+zshpath=$(which zsh)
+if [[ ! "$zshpath" = "$SHELL" ]]; then
+	log_info zsh "Changing default shell..."
+	chsh -s $zshpath 
+fi
+log_success zsh "Default shell is zsh!"
+
 install_ohmyzsh() {
     log_info ohmyzsh "Installing..."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
@@ -17,31 +25,38 @@ log_info ohmyzsh "Checking installation..."
 log_success ohmyzsh "Installed!"
 
 install_theme() {
+	log_info ohmyzsh "Installing theme '$1'..."
     name=$1
     repo=$2
     git clone "$repo" "$CUSTOMDIR/themes/$name"
 }
 
 install_plugin() {
+	log_info ohmyzsh "Installing plugin '$1'..."
     name=$1
     repo=$2
     git clone "$repo" "$CUSTOMDIR/plugins/$name"
 }
 
 check_theme_installed() {
+	log_info ohmyzsh "Checking theme '$1' is installed..."
     name=$1
     repo=$2
     [[ -d $CUSTOMDIR/themes/$name ]] || install_theme $name $repo
+	log_success ohmyzsh "Theme '$1' is installed."
 }
 
 check_plugin_installed() {
+	log_info ohmyzsh "Checking plugin '$1' is installed..."
     name=$1
     repo=$2
     [[ -d $CUSTOMDIR/plugins/$name ]] || install_plugin $name $repo
+	log_success ohmyzsh "Plugin '$1' is installed."
 }
 
 check_theme_installed powerlevel10k https://github.com/romkatv/powerlevel10k
 
+log_info ohmyzsh "Adding zsh configuration..."
 ln -fs `pwd`/zsh/p10k.zsh "$CUSTOMDIR/p10k.zsh"
 ln -fs `pwd`/zsh/aliases.zsh "$CUSTOMDIR/aliases.zsh"
 ln -fs `pwd`/zsh/zshrc "${HOME}/.zshrc"
