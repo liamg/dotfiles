9a91066a05e8ad04233135f0fd44efd33fe86a8c
Liam Galvin <liamgalvin@protonmail.com>
2021-08-19 20:49:50 +0100

    Make project sessions work nicely

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,17 @@
 alias vim=nvim
 
+__custom__load_session () {
+	nvim . +"SLoad! $1"
+}
+
+__custom__create_session () {
+	nvim . +"SSave! $1"
+}
+
 e () {
-	(z $1 && nvim . +NvimTreeOpen) || echo "Could not find $1."
+	z $1 || return
+	session=$(basename `pwd`)
+	[[ -f ~/.local/share/nvim/session/$session ]] && __custom__load_session $session && return
+	__custom__create_session $session
 }
 
