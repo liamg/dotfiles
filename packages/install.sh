b25545bea6f0ac69c869d05fb2bb562dbe7ec452
Liam Galvin <liamgalvin@protonmail.com>
2021-09-05 20:52:58 +0100

    Add go packages installer

--- packages/install.sh
+++ packages/install.sh
@@ -12,3 +12,9 @@ which spt >/dev/null 2>&1 || snap install spt 2>/dev/null
 
 ln -sf $(which fdfind) ~/.local/bin/fd
 
+while IFS= read -r line
+do
+ [[ -z "$line" ]] || go install github.com/${line}@latest
+done < packages/golist
+
+
